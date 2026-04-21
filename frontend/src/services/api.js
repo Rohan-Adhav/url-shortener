fce438864e7c0f7f4ee2faeb18845cc7499fba6e
Rohan  Adhav <155009973+Rohan-Adhav@users.noreply.github.com>
2026-04-21 15:00:10 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,14 +3,14 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_BASE_URL
 
 const shortenUrl = async (originalUrl)=>{
-    let res = await axios.post(`${baseURL}/url/shorten`,{
+    let res = await axios.post(`${baseURL}/api/url/shorten`,{
         originalUrl
     })
     return res.data
 }
 
 const getStats = async (shortCode)=>{
-    let res = await axios.get(`${baseURL}/url/${shortCode}/stats`)
+    let res = await axios.get(`${baseURL}/api/url/${shortCode}/stats`)
     return res.data.data
 }
 
